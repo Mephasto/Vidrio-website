d22742b6a3fc58f7fa985f643100c84759cbd549
Ezequiel Rabinovich <erabinovich@gmail.com>
2013-02-14 14:46:44 -0300

    Improves the title animation

--- static/js/script.js
+++ static/js/script.js
@@ -1,34 +1,31 @@
 /* Author: YOUR NAME HERE
 */
 
-$(document).ready(function() {   
+$(document).ready(function() {
+    'use strict';
 
-    // var socket = io.connect();
+    var mailListInput, animate, changeColor, elem;
 
-    // $('#sender').bind('click', function() {
-    //     socket.emit('message', 'Message Sent on ' + new Date());
-    // });
+    elem = $('header h2');
 
-    // socket.on('server_message', function(data){
-    //     $('#receiver').append('<li>' + data + '</li>');
-    // });
+    animate = function () {
+        elem.css('background-position', Math.floor(Math.random()*255) + 'px ' + Math.floor(Math.random()*255) + 'px' );
+        requestAnimationFrame(animate, elem);
+    };
+    changeColor = function () {
+        elem.css('background-color', '#'+Math.floor(Math.random()*16777215).toString(16));
+        elem.css('color', '#'+Math.floor(Math.random()*16777215).toString(16));
+    };
 
-    var headerAnim = setInterval(mycode, 1000);
-    function mycode() {
-        $('header h2').css('background-color', '#'+Math.floor(Math.random()*16777215).toString(16));
-        $('header h2').css('color', '#'+Math.floor(Math.random()*16777215).toString(16));
-        //$('header h1').css('color', '#'+Math.floor(Math.random()*16777215).toString(16));
-    }
+    elem.on('transitionend webkitTransitionEnd oTransitionEnd otransitionend', changeColor);
+    changeColor();
+    animate();
 
-    var bgAnim = setInterval(mycodeB, 150);
-    function mycodeB() {
-        $('header h2').css('background-position', Math.floor(Math.random()*255) + 'px ' + Math.floor(Math.random()*255) + 'px' );
-    }
 
     $('#datepicker').datepicker();
     $('#timepicker').timepicker();
 
-    var mailListInput = $('#mailListInput').attr('value');
+    mailListInput = $('#mailListInput').attr('value');
     $('#mailListInput').focusin(function(){
         if($(this).attr('value') == mailListInput){
             $(this).attr('value','');
